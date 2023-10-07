3046db67643e6416dc9a78dce3e8ca8b739c91ed
Timothy Buchak <timothy.buchak@gmail.com>
2023-10-06 23:50:31 -0400

    setting up online server

--- Stats/database/statCreate.sh
+++ Stats/database/statCreate.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export PGPASSWORD='postgres1'
+export PGPORT=15432
 BASEDIR=$(dirname $0)
 DATABASE=2kstats
 for i in 18 17 16 15 14 13 12 11 10 9
@@ -9,8 +10,9 @@ do
     if [ -d "$file" ]
     then
         echo "Postgres version $i found."
-        "$FILEPATH/psql.exe" -U postgres -d $DATABASE -f "$BASEDIR/schema.sql" &&
-        "$FILEPATH/psql.exe" -U postgres -d $DATABASE -f "$BASEDIR/data.sql"
+        createdb -U postgres -p $PGPORT $DATABASE &&
+        "$FILEPATH/psql.exe" -U postgres -p $PGPORT -d $DATABASE -f "$BASEDIR/schema.sql" &&
+        "$FILEPATH/psql.exe" -U postgres -p $PGPORT -d $DATABASE -f "$BASEDIR/data.sql"
         break
     fi
 done
